6817454bcdd45ac88759fb82ad4282c78165d98b
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-02-24 16:07:14 +0100

    Merged default into dev

--- lib/tpl/frontend.php
+++ lib/tpl/frontend.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
+	<head>
 		<meta charset="<?php echo get_bloginfo( 'charset' ); ?>" />
 		<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
 		<?php wp_head(); ?>
 	</head>
-    <body <?php body_class(); ?>>
-		<header>
+	<body <?php body_class(); ?>>
 		<?php echo do_shortcode( '[sv_navigation_primary inline="true"]' ); ?>
-		</header>
