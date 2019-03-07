0233b64499f0f1665110a40524370219cc9f36bc
Matthias Reuter <info@straightvisions.com>
2019-03-07 02:53:40 +0100

    MIGRATE v3

--- sv_header.php
+++ sv_header.php
@@ -23,6 +23,11 @@ class sv_header extends init {
 
 		// Shortcodes
 		add_shortcode( $this->get_module_name(), array( $this, 'shortcode' ) );
+
+		$this->scripts_queue['frontend']			= static::$scripts->create( $this )
+			->set_ID('frontend')
+			->set_path( 'lib/css/frontend.css' )
+			->set_inline(true);
 	}
 
 	public function shortcode( $settings, $content = '' ) {
@@ -35,9 +40,9 @@ class sv_header extends init {
 		);
 
 		// Load Styles
-		static::$scripts->create( $this )
-		                ->set_path( 'lib/css/frontend.css' )
-		                ->set_inline($settings['inline']);
+		$this->scripts_queue['frontend']
+			->set_inline($settings['inline'])
+			->set_is_enqueued();
 
 		ob_start();
 		include( $this->get_path( 'lib/tpl/frontend.php' ) );
