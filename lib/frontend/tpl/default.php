4801465365c3dfd322cc55e4407d0f5400907c73
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2019-04-09 10:25:21 +0200

    Removed image out of header and added mobile menu toggle button stylings

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -9,10 +9,6 @@
     <body <?php body_class(); ?>>
         <header class="<?php echo $this->get_prefix(); ?>">
             <div class="<?php echo $this->get_prefix( 'bar' ); ?> sv_common_container">
-                <a href="<?php echo get_home_url(); ?>">
-                    <img src="<?php echo $this->get_url( 'lib/frontend/img/logo.svg' ); ?>" alt="Logo"
-                         class="<?php echo $this->get_prefix( 'branding' ); ?>">
-                </a>
                 <?php echo do_shortcode( '[sv_navigation location="' . $this->get_module_name() . '" show_images="1"]' ); ?>
                 <aside class="<?php echo $this->get_prefix( 'sidebar' ); ?>">
                     <?php echo do_shortcode( '[sv_sidebar id="' . $this->get_module_name() . '"]' ); ?>
