d578f9b19afb9564ecb218bfdf509d15a3e76381
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-31 14:34:20 +0200

    Added custom header and custom header image support

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -17,7 +17,9 @@
 		<?php if ( $this->get_setting( 'branding' )->run_type()->get_data() ) { ?>
 			<div class="<?php echo $this->get_prefix( 'branding' ); ?>">
 				<?php
-                    if ( get_custom_logo() ) {
+                    if ( get_header_image() ) {
+                    	echo '<a href="' . get_home_url() . '"><img src="' . get_header_image() . '" alt="' . get_bloginfo( 'title' ) . '" /></a>';
+					} elseif ( get_custom_logo() ) {
                         echo get_custom_logo();
                     } else {
                         $title = empty( $this->get_setting( 'branding_title' )->run_type()->get_data() )
