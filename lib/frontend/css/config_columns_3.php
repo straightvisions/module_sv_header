0fa1a284c06d6ef1f7cad2887dfd16aeeb47a6a6
dennisheiden <heiden@straightvisions.com>
2019-11-14 08:07:36 +0100

    fix - css 3-columns alignment

--- lib/frontend/css/config_columns_3.php
+++ lib/frontend/css/config_columns_3.php
@@ -36,45 +36,43 @@
     }
 <?php }elseif($box_content_alignment == 'spread'){ // SPREAD / SPACE BETWEEN ------------- ?>
     .sv100_sv_header .sv100_sv_header_bar {
-    justify-content: space-between;
+        justify-content: space-between;
     }
 
     .sv100_sv_header .sv100_sv_header_bar > *{
-    flex: 0 1 auto;
-    width: auto;
+        justify-content: center;
+        flex: 0 1 32%;
+        width: auto;
+    }
+
+    .sv100_sv_header .sv100_sv_header_bar > *:first-child{
+        justify-content: flex-start;
     }
 
     .sv100_sv_header .sv100_sv_header_bar > *:last-child{
-    justify-content: flex-end;
-    }
-
-    .sv100_sv_header .sv100_sv_header_bar > *:nth-child(2){
-    justify-content: center;
+        justify-content: flex-end;
     }
 
 <?php }else{  // CENTER / FLEX START ------------- ?>
     .sv100_sv_header .sv100_sv_header_bar {
-    justify-content: center;
+        justify-content: center;
     }
 
     .sv100_sv_header .sv100_sv_header_bar > *{
-    margin-left: 5%;
-    }
-
-    .sv100_sv_header .sv100_sv_header_bar > *{
-    flex: 0 1 32%;
-    width: auto;
+        justify-content: center;
+        flex: 0 1 32%;
+        width: auto;
+        margin-left: 5%;
     }
 
     .sv100_sv_header .sv100_sv_header_bar > *:first-child{
-    justify-content: flex-end;
-    margin-left: 0;
+        justify-content: flex-end;
+        margin-left: 0;
     }
 
-    <?php if($navigation_order == 1){ ?>
-        .sv100_sv_header .sv100_sv_header_bar .sv100_sv_navigation_sv_header_primary{
-        justify-content: flex-end;
-        }
-    <?php } ?>
+    .sv100_sv_header .sv100_sv_header_bar > *:last-child{
+        justify-content: flex-start;
+    }
+
 
 <?php } ?>
