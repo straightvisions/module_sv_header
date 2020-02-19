2f49301705b1804bb462f1c081f3d062ba50cb49
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2020-02-19 13:47:42 +0100

    Fixed that logo_height_mobile setting was not working

--- lib/frontend/css/config/branding.php
+++ lib/frontend/css/config/branding.php
@@ -10,8 +10,8 @@ height: <?php echo $branding_logo_height < 1 ? 'auto' : $branding_logo_height . 
 
 .sv100_sv_header .sv100_sv_header_branding img {
 width: <?php echo $branding_logo_width_mobile < 1 ? 'auto' : $branding_logo_width_mobile . 'px'; ?>;
-height: <?php echo $branding_logo_height < 1 ? '100%' : $branding_logo_height . 'px'; ?>;
-max-height: <?php echo $branding_logo_height < 1 ? '60px' : $branding_logo_height . 'px'; ?>;
+height: <?php echo $branding_logo_height_mobile < 1 ? '100%' : $branding_logo_height_mobile . 'px'; ?>;
+max-height: <?php echo $branding_logo_height_mobile < 1 ? '60px' : $branding_logo_height_mobile . 'px'; ?>;
 }
 
 @media ( min-width: 1350px ) {
