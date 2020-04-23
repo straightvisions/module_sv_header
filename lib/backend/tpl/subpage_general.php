d49c89ccf16bdd537d5c0d3c0a2784a1f28077e2
Dennis Heiden <dennis-heiden@straightvisions.com>
2020-04-23 16:54:21 +0200

    add - settings - inner alignment

--- lib/backend/tpl/subpage_general.php
+++ lib/backend/tpl/subpage_general.php
@@ -9,6 +9,13 @@
         	echo $module->get_setting( 'sidebar_order' )->form();
 		?>
 	</div>
+    <div class="sv_setting_flex">
+		<?php
+            echo $module->get_setting( 'branding_alignment' )->form();
+            echo $module->get_setting( 'navigation_alignment' )->form();
+            echo $module->get_setting( 'sidebar_alignment' )->form();
+		?>
+    </div>
     <h3 class="divider"><?php _e( 'Position & Alignment', 'sv100' ); ?></h3>
     <div class="sv_setting_flex">
 		<?php
