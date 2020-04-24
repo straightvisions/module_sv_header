fdb049c4fbc4da1f88be14a34446577d193eaa3f
Dennis Heiden <dennis-heiden@straightvisions.com>
2020-04-24 15:00:07 +0200

    remove unnecessary code + add array check for responsive sidebar

--- lib/backend/tpl/subpage_sidebar.php
+++ lib/backend/tpl/subpage_sidebar.php
@@ -3,22 +3,4 @@
     <div class="sv_setting_flex">
         <?php echo $module->get_setting( 'sidebar_active' )->form(); ?>
     </div>
-	<h3 class="divider"><?php _e( 'Sidebar Alignments', 'sv100' ); ?></h3>
-    <?php
-		// Sidebars - Alignment Settings
-		$i = 0;
-    foreach ( $module->get_module( 'sv_sidebar' )->get_sidebars( $module ) as $sidebar ) {
-        echo $i === 0 ? '<div class="sv_setting_flex">' : '';
-
-        echo $module->get_setting( $sidebar['id'] )->form();
-
-        $i++;
-
-        if ( $i === 3 || $i === $count ) {
-            $i = 0;
-
-            echo '</div>';
-        }
-    }
-	?>
 </div>
--- lib/frontend/css/config.php
+++ lib/frontend/css/config.php
@@ -17,7 +17,8 @@
     if( $script->get_parent()->get_module('sv_header_menu')->get_setting( 'active' )->get_data() ){
         include( $script->get_parent()->get_path( 'lib/frontend/css/config/navigation.php' ) );
     }
-    if( $script->get_parent()->get_setting( 'sidebar_active' )->get_data() == 1
+
+    if( in_array('1', $script->get_parent()->get_setting( 'sidebar_active' )->get_data())
     && $script->get_parent()->get_root()->get_module( 'sv_sidebar' )
     && $script->get_parent()->get_root()->get_module( 'sv_sidebar' )->load( array( 'id' => $script->get_parent()->get_module_name().'_sidebar', ) )
     ){
