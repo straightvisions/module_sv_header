b9c1e9f04e4158d709cff52877d539d8b0e02ccb
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-24 17:53:29 +0200

    FIX ADMINBAR OFFSET

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,26 +1,5 @@
 <?php
 
-	// calculate margin for adminbar when header has position fixed
-	$top_set	= false;
-	$top		= array('top' => array());
-	foreach($module->get_setting('position')->get_data() as $screen_size => $value){
-		if($value == 'fixed' || $value == 'absolute' || $value == 'sticky'){
-			$top_set = true;
-			if($screen_size == 'desktop') {
-				$top['top'][$screen_size] = '32px';
-			}else{
-				$top['top'][$screen_size] = '46px';
-			}
-		}
-	}
-
-	if($top_set) {
-		echo $_s->build_css(
-			'.admin-bar .sv100_sv_header_wrapper',
-			$top
-		);
-	}
-
 	echo $_s->build_css(
 		'.sv100_sv_header_wrapper',
 		array_merge(
