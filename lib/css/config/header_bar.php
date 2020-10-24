818e0e5a0cce8293dc3ef6b1ce5b70858a96573f
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-10-24 13:29:01 +0200

    FIX COLUMN HEIGHT ON SAFARI (MOBILE)

--- lib/css/config/header_bar.php
+++ lib/css/config/header_bar.php
@@ -24,7 +24,7 @@
 	$properties['flex']   = $_s->get_breakpoints();
 
 	foreach( $properties['flex'] as $key => &$value){
-		$value = '1 1 0';
+		$value = '1 1 auto';
 		if(isset($container_alignment[$key]) && !in_array($container_alignment[$key] , ['center', 'spread'])){
 			$value = '0 1 auto';
 		}
