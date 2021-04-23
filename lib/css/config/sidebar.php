5d946dd06fe66155b8b44e9c23051225e1320d65
Dennis Heiden <dennis-heiden@straightvisions.com>
2021-04-23 14:46:32 +0200

    fix order again

--- lib/css/config/sidebar.php
+++ lib/css/config/sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 	$properties = array();
 
 	$sidebar_active		= $module->get_setting('sidebar_active')->get_data();
@@ -30,11 +31,11 @@
 		array_merge(
 			$properties,
 			$module->get_setting('sidebar_max_width')->get_css_data('max-width'),
-			$module->get_setting('sidebar_alignment')->get_css_data('justify-content'),
-			$module->get_setting('sidebar_order')->get_css_data('order')
+			$module->get_setting('sidebar_alignment')->get_css_data('justify-content')
+			
 		)
 	);
-
+	
 	echo $_s->build_css(
 		'.sv100_sv_header .sv100_sv_header_bar > .sv100_sv_header_sidebar .sv100_sv_sidebar_sv_header_sidebar',
 		array_merge(
